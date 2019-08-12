4995e24298b558a2e6ac57bc452ad9586bc8f594
Matt Williams <matt.seongjun@gmail.com>
2019-08-12 13:53:46 -0400

    Add tabs for future language selection

--- src/components/texts/texts-container.js
+++ src/components/texts/texts-container.js
@@ -1,13 +1,18 @@
 import React from "react";
 import TextsView from "./texts-view";
-import { Item, Container, Pagination } from 'semantic-ui-react'
+import { Item, Container, Pagination, Tab } from 'semantic-ui-react'
 
 class TextsContainer extends React.Component {
     render() {
         let { texts } = this.props
-
+        const panes = [
+            { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+            { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+            { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+        ]
         return (
             <Container>
+                {/* <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} /> */}
                 <Item.Group>
                     {texts.map(text => <TextsView key={text.id} text={text} />)}
                 </Item.Group>
