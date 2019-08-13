8f4a59f98d84a93594eca1c40a99929f1cba524c
Matt Williams <matt.seongjun@gmail.com>
2019-08-13 14:10:04 -0400

    Setup boilerplate for authentication middleware

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
-
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 const httpLink = createHttpLink({
     // The path to your GraphQL powered server
@@ -16,8 +17,19 @@ const httpLink = createHttpLink({
     uri: 'http://localhost:4000/graphql'
 })
 
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+        return {
+            headers: {
+              ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+            }
+        } 
+    })
+
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
 
