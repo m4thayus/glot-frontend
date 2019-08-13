fe0551e76621a3bcf9e0f81bb5e67a5c4313dafb
Matt Williams <matt.seongjun@gmail.com>
2019-08-13 11:34:35 -0400

    Add home page placeholder

---
+++ src/components/home/home-container.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeView from "./home-view";
+
+class HomeContainer extends React.Component {
+    render() {
+        return (
+            <HomeView />
+        )
+    }
+}
+
+export default HomeContainer
---
+++ src/components/home/home-view.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function HomeView(props){
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam laoreet dui quis diam facilisis, in pharetra lorem vehicula. Duis volutpat facilisis massa, quis dictum dolor sodales a. Donec vitae enim vitae augue laoreet suscipit eget at justo. Curabitur faucibus nunc elementum, tincidunt eros ut, elementum massa. Pellentesque sit amet leo aliquam, commodo arcu et, commodo mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam gravida mollis velit, vel placerat tortor pellentesque in.</p>
+            <p>Donec nec lacus non metus posuere facilisis. Cras hendrerit lacus at ligula convallis lobortis. Fusce consectetur elementum tortor sit amet commodo. Etiam mattis augue nec felis auctor vehicula eget at mauris. Donec ut viverra diam, at commodo purus. Mauris quis molestie ante. Sed ut feugiat tellus, vel suscipit nulla. Ut congue, mi id laoreet consequat, nisl nisl gravida ipsum, congue facilisis tortor sem ut quam. Mauris ac efficitur augue. In bibendum euismod risus eu molestie. Fusce sed mauris dui. Mauris nec augue non justo auctor accumsan vitae commodo tortor. Phasellus vitae sem eu diam porta ultrices gravida id est.</p>
+            <p>Fusce pulvinar libero consequat vehicula gravida. Duis sed velit ipsum. Fusce eleifend urna sit amet purus dapibus, sed aliquet odio egestas. Maecenas feugiat nunc sapien. Nunc dolor magna, placerat et euismod id, pharetra nec neque. In fringilla neque non lobortis consequat. Vestibulum pellentesque, velit eget sollicitudin mattis, mauris nisl tincidunt tortor, nec fermentum enim risus sit amet eros. Fusce fermentum, felis nec semper tincidunt, velit leo tincidunt leo, nec rutrum dui lacus euismod leo. Curabitur purus ex, euismod id viverra aliquet, elementum varius libero. In gravida, ante nec iaculis iaculis, tortor mi porttitor erat, nec egestas massa ante quis lectus. Nam congue interdum ipsum, sed hendrerit arcu semper vitae. Duis sodales orci turpis, vel rhoncus orci pellentesque vel. </p>
+        </React.Fragment>
+    )
+}
+
+export default HomeView
