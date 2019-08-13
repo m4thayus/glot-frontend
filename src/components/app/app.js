30a21624b42d285622d83f0da1f6b3f087e00935
Matt Williams <matt.seongjun@gmail.com>
2019-08-13 11:36:08 -0400

    Routing to home seems to be working

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,6 +4,7 @@ import './app.css';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import { Segment, Divider } from 'semantic-ui-react';
 import NavBar from '../nav-bar'
+import Home from '../home'
 import Texts from '../texts'
 import Translations from '../translations'
 
@@ -14,6 +15,7 @@ function App() {
                 <Segment>
                     <NavBar />
                     <Divider horizontal />
+                    <Route exact path="/" component={Home} />
                     <Route path="/texts" component={Texts} />
                     <Route path="/translations" component={Translations} />
                 </Segment>
