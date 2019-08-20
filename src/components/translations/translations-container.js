76743bda21a104b888e874cea381ebf302f0ca17
Matt Williams <matt.seongjun@gmail.com>
2019-08-20 14:39:18 -0400

    Conditionally render translations

--- src/components/translations/translations-container.js
+++ src/components/translations/translations-container.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TranslationsView from "./translations-view";
-import { Item, Container, Pagination, Segment } from 'semantic-ui-react'
+import { Item, Container, Pagination, Segment, Header, Button} from 'semantic-ui-react'
+import { Link } from "react-router-dom";
 
 class TranslationsContainer extends React.Component {
     render() {
@@ -9,10 +10,29 @@ class TranslationsContainer extends React.Component {
         return (
             <Container>
                 <Segment raised>
-                    <Item.Group divided>
-                        {translations.map(translation => <TranslationsView key={translation.id} translation={translation} />)}
-                    </Item.Group>
-                    <Pagination secondary defaultActivePage={1} disabled totalPages={5} />
+                    { translations.length === 0 
+                        ?
+                            <React.Fragment>
+                                <Header>That's unfortunate...</Header>
+                                <Segment textAlign="center" basic>
+                                    <Button
+                                        primary
+                                        animated="fade"
+                                        as={Link}
+                                        to='/texts'>
+                                        <Button.Content visible>You Haven't Written Any Yet!</Button.Content>
+                                        <Button.Content hidden>Find A Text To Translate</Button.Content>
+                                    </Button>
+                                </Segment>
+                            </React.Fragment>
+                        :
+                            <React.Fragment>
+                                <Item.Group divided>
+                                    {translations.map(translation => <TranslationsView key={translation.id} translation={translation} />)}
+                                </Item.Group>
+                                <Pagination secondary defaultActivePage={1} disabled totalPages={5} />
+                            </React.Fragment>
+                    }
                 </Segment>
             </Container>
         )
