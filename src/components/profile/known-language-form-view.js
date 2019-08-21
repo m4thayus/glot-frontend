4a5ce1609ef62a5c109561fccb97bf26d495dd09
Matt Williams <matt.seongjun@gmail.com>
2019-08-21 08:21:22 -0400

    add structure for future difficulty select

--- src/components/profile/known-language-form-view.js
+++ src/components/profile/known-language-form-view.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Form, Modal, Segment} from "semantic-ui-react";
+import KnownLanguageMutation from "./known-language-mutation";
 
 function LanguageFormView(props){
 
@@ -11,16 +12,31 @@ function LanguageFormView(props){
         }
     ))
 
+    // let difficulties = props.selectedLanguage.difficulties.map(difficulty => (
+    //     {
+    //         key: difficulty.value,
+    //         text: difficulty.description,
+    //         value: difficulty.value
+    //     }
+    // ))
+
     return (
         <React.Fragment>
             <Modal.Header>Add A Language</Modal.Header>
             <Modal.Description>
                 <Segment padded='very' basic>
                     <Form>
-                        <Form.Dropdown placeholder='Languages' fluid multiple selection options={languages} />
+                        <Form.Dropdown placeholder='Language' fluid selection options={languages} />
+                        {/* <Form.Dropdown placeholder='Difficulty' fluid selection options={difficulties} /> */}
                     </Form>
                 </Segment>
             </Modal.Description>
+            <Modal.Actions>
+                <KnownLanguageMutation
+                    // first_name={first_name}
+                    // last_name={last_name}
+                />
+            </Modal.Actions>
         </React.Fragment>
     )
 }
