d574442d34733aca5bf091086c229817dd89f46e
Matt Williams <matt.seongjun@gmail.com>
2019-08-21 18:09:09 -0400

    Hide faqs till after demo

--- src/components/dashboard/dashboard-view.js
+++ src/components/dashboard/dashboard-view.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Header, Statistic, Accordion, Button, Segment } from "semantic-ui-react";
+import { Header, Statistic, Button, Segment } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import ProgressView from "./progress-view";
 
@@ -18,45 +18,45 @@ function DashboardView(props){
         {key: 'pay', label: 'Total Earned', value: `$${total_pay}`}
     ]
 
-    let faqs =[
-        {
-            key: 'what-is-dog',
-            title: 'What is a dog?',
-            content: [
-                'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome',
-                'guest in many households across the world.',
-                ].join(' '),
-        },
-        {
-            key: 'kinds-of-dogs',
-            title: 'What kinds of dogs are there?',
-            content: [
-                'There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog',
-                'that they find to be compatible with their own lifestyle and desires from a companion.',
-                ].join(' '),
-        },
-        {
-            key: 'acquire-dog',
-            title: 'How do you acquire a dog?',
-            content: {
-                content: (
-                    <div>
-                    <p>
-                    Three common ways for a prospective owner to acquire a dog is from
-                    pet shops, private owners, or shelters.
-                    </p>
-                    <p>
-                    A pet shop may be the most convenient way to buy a dog. Buying a dog
-                    from a private owner allows you to assess the pedigree and
-                    upbringing of your dog before choosing to take it home. Lastly,
-                    finding your dog from a shelter, helps give a good home to a dog who
-                    may not find one so readily.
-                    </p>
-                    </div>
-                ),
-            },
-        },
-    ]
+    // let faqs =[
+    //     {
+    //         key: 'what-is-dog',
+    //         title: 'What is a dog?',
+    //         content: [
+    //             'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome',
+    //             'guest in many households across the world.',
+    //             ].join(' '),
+    //     },
+    //     {
+    //         key: 'kinds-of-dogs',
+    //         title: 'What kinds of dogs are there?',
+    //         content: [
+    //             'There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog',
+    //             'that they find to be compatible with their own lifestyle and desires from a companion.',
+    //             ].join(' '),
+    //     },
+    //     {
+    //         key: 'acquire-dog',
+    //         title: 'How do you acquire a dog?',
+    //         content: {
+    //             content: (
+    //                 <div>
+    //                 <p>
+    //                 Three common ways for a prospective owner to acquire a dog is from
+    //                 pet shops, private owners, or shelters.
+    //                 </p>
+    //                 <p>
+    //                 A pet shop may be the most convenient way to buy a dog. Buying a dog
+    //                 from a private owner allows you to assess the pedigree and
+    //                 upbringing of your dog before choosing to take it home. Lastly,
+    //                 finding your dog from a shelter, helps give a good home to a dog who
+    //                 may not find one so readily.
+    //                 </p>
+    //                 </div>
+    //             ),
+    //         },
+    //     },
+    // ]
 
     let textsProgress = user.texts.map(text => (
         <ProgressView key={text.id} item={text} />
@@ -106,10 +106,10 @@ function DashboardView(props){
                     {user.translations.length === 0 ? writeTranslation : translationsProgress}
                 </Segment>
             </Segment>
-            <Segment>
+            {/* <Segment>
                 <Header>FAQs</Header>
                 <Accordion defaultActiveIndex={0} panels={faqs} />
-            </Segment>
+            </Segment> */}
         </Segment.Group>
     )
 }
