a8ec06559ab1a34a1f00ceb24ad9d6cd97a98646
Matt Williams <matt.seongjun@gmail.com>
2019-08-21 22:26:02 -0400

    Show disabled button; implement after demo

--- src/components/profile-form/update-user-mutation.js
+++ src/components/profile-form/update-user-mutation.js
@@ -1,13 +1,13 @@
 import React from "react"
-import { Mutation } from 'react-apollo'
-import { loader } from 'graphql.macro';
+// import { Mutation } from 'react-apollo'
+// import { loader } from 'graphql.macro';
 import { Button } from "semantic-ui-react";
 
-const UPDATE_USER_MUTATION = loader('../graphql/update-user-mutation.graphql');
+// const UPDATE_USER_MUTATION = loader('../graphql/update-user-mutation.graphql');
 
 function UserMutation(props){
     return (
-        <div>Mutate</div>
+        <Button disabled>Submit</Button>
     )
 }
 
