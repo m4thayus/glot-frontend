d8062ac7aa1983ecec4063422ad0b700b83e33a3
Matt Williams <matt.seongjun@gmail.com>
2019-08-21 22:32:22 -0400

    Reduce steps

--- src/components/dashboard/progress-view.js
+++ src/components/dashboard/progress-view.js
@@ -4,9 +4,7 @@ import { Header, Progress, Segment } from "semantic-ui-react";
 const statusToNum = string => {
     switch (string) {
         case 'complete':
-            return 4       
-        case 'under-review':
-            return 3        
+            return 3       
         case 'pending':
             return 2            
         default:
@@ -19,7 +17,7 @@ function ProgressView(props){
     return (
         <Segment>
             <Header>{item.title}</Header>
-            <Progress value={statusToNum(item.status)} total={4} progress='ratio' size="medium" />
+            <Progress value={statusToNum(item.status)} total={3} progress='ratio' size="medium" />
         </Segment>
     )
 }
