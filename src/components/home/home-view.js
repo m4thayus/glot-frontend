dcf1a4ede1a929577ec22f05d2e849876aa05eed
Matt Williams <matt.seongjun@gmail.com>
2019-09-23 11:53:25 -0400

    Revamp home (splash) page

--- src/components/home/home-view.js
+++ src/components/home/home-view.js
@@ -1,13 +1,22 @@
 import React from "react"
-import { Segment } from "semantic-ui-react";
+import { Segment, Header, Button, Icon } from "semantic-ui-react";
 
 function HomeView(props){
     return (
         <Segment raised>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam laoreet dui quis diam facilisis, in pharetra lorem vehicula. Duis volutpat facilisis massa, quis dictum dolor sodales a. Donec vitae enim vitae augue laoreet suscipit eget at justo. Curabitur faucibus nunc elementum, tincidunt eros ut, elementum massa. Pellentesque sit amet leo aliquam, commodo arcu et, commodo mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam gravida mollis velit, vel placerat tortor pellentesque in.</p>
-            <p>Donec nec lacus non metus posuere facilisis. Cras hendrerit lacus at ligula convallis lobortis. Fusce consectetur elementum tortor sit amet commodo. Etiam mattis augue nec felis auctor vehicula eget at mauris. Donec ut viverra diam, at commodo purus. Mauris quis molestie ante. Sed ut feugiat tellus, vel suscipit nulla. Ut congue, mi id laoreet consequat, nisl nisl gravida ipsum, congue facilisis tortor sem ut quam. Mauris ac efficitur augue. In bibendum euismod risus eu molestie. Fusce sed mauris dui. Mauris nec augue non justo auctor accumsan vitae commodo tortor. Phasellus vitae sem eu diam porta ultrices gravida id est.</p>
-            <p>Fusce pulvinar libero consequat vehicula gravida. Duis sed velit ipsum. Fusce eleifend urna sit amet purus dapibus, sed aliquet odio egestas. Maecenas feugiat nunc sapien. Nunc dolor magna, placerat et euismod id, pharetra nec neque. In fringilla neque non lobortis consequat. Vestibulum pellentesque, velit eget sollicitudin mattis, mauris nisl tincidunt tortor, nec fermentum enim risus sit amet eros. Fusce fermentum, felis nec semper tincidunt, velit leo tincidunt leo, nec rutrum dui lacus euismod leo. Curabitur purus ex, euismod id viverra aliquet, elementum varius libero. In gravida, ante nec iaculis iaculis, tortor mi porttitor erat, nec egestas massa ante quis lectus. Nam congue interdum ipsum, sed hendrerit arcu semper vitae. Duis sodales orci turpis, vel rhoncus orci pellentesque vel. </p>
+            <Header>Welcome to glot!</Header>
+            <p>glot is a place to translate for fun and profit, connecting translators with those who need translations.</p>
+            <p><strong>Please login or create an account to access the full site.</strong></p>
+            <p>This site was developed by <a href="https://conceptualsoup.blog/about">@m4thayus</a>, a full stack web developer. To view his blog follow the link below:</p>
+            <Button
+                as="a"
+                href="https://conceptualsoup.blog"
+            >
+                <Icon name="arrow left"/>
+                Back to Conceptual Soup
+            </Button>
+            {/* <p>glot is still undergoing development so stay tuned.</p> */}
+
         </Segment>
     )
 }
