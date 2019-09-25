5559a423669e5479063475368b5d21d3f5049494
Matt Williams <matt.seongjun@gmail.com>
2019-09-25 13:43:49 -0400

    Add demo credentials to home page

--- src/components/home/home-view.js
+++ src/components/home/home-view.js
@@ -7,6 +7,11 @@ function HomeView(props){
             <Header>Welcome to glot!</Header>
             <p>glot is a place to translate for fun and profit, connecting translators with those who need translations.</p>
             <p><strong>Please login or create an account to access the full site.</strong></p>
+            <p>For a demonstration, please use the following credentials:</p>
+            <ul>
+                <li>User: demo</li>
+                <li>Pass: 123</li>
+            </ul>
             <p>This site was developed by <a href="https://github.com/m4thayus">@m4thayus</a>, a full stack web developer. To view his blog follow the link below:</p>
             <Button
                 as="a"
